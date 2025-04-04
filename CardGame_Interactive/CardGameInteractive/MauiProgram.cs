4f45eeac8667fad92d15fa5bfac50e0723102168
GMan2605 <gavinblackie2026@gmail.com>
2025-04-04 11:42:19 -0400

    Added a custom font for the game's title

--- CardGame_Interactive/CardGameInteractive/MauiProgram.cs
+++ CardGame_Interactive/CardGameInteractive/MauiProgram.cs
@@ -11,6 +11,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Jacquard24-Regular.ttf", "Jacquard"); // Add a custom font!
             });
 
         return builder.Build();
